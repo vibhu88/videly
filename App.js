e71652c6898c3d6ac2ccc4bb6c255cada6d6b203
vibhu88 <vibhu.eclipse@gmail.com>
2020-08-01 02:41:35 +0530

    Added Put Validations, Post and Delete requests

--- App.js
+++ App.js
@@ -13,12 +13,12 @@ let genres = [  {id: 1, gr: "Rock"},
                 {id: 6, gr: "Romantic"}
              ]   
 
-
+// Generic GET
 app.get('/api/genres/', (req, res) => {
     res.status(200).send(genres);
 })
 
-
+// GET to fetch specific genre
 app.get('/api/genres/:id', (req, res) => {
     const genre = genres.find(element => element.id === parseInt(req.params.id));
     if(!genre)
@@ -31,16 +31,75 @@ app.get('/api/genres/:id', (req, res) => {
         }
 })
 
+// POST to save a genre received in request body with validation
 app.post('/api/genres/', (req, res) => {
-    const genre = {
-                    id: genres.length + 1,
-                    gr: req.body.name
-                  };
-    genres.push(genre);
-    res.send(genre);
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+    
+    const { error, value } = schema.validate(req.body);
+    if (!req.body.name || req.body.name.length < 3) 
+        {
+            res.status(400).send(error.details[0].message);
+            return;
+        }   
+    else
+        {
+            const genre = 
+            {
+                id: genres.length + 1,
+                gr: req.body.name
+            }
+            genres.push(genre);
+            res.send(genre);
+        }    
+})  
+
+// PUT to update a genre for which id is received in URL and genre is received in request body with validation
+app.put('/api/genres/:id', (req, res) => {
+
+    const genre = genres.find(element => element.id === parseInt(req.params.id));
+    if(!genre)
+        {
+            res.status(404).send("Genre Not Found");
+            return;
+        }
+    else
+        {
+            const schema = Joi.object({
+                name: Joi.string().min(3).required()
+            })
+            
+            const { error, value } = schema.validate(req.body);
+            if (!req.body.name || req.body.name.length < 3) 
+                {
+                    res.status(400).send(error.details[0].message);
+                    return;
+                }   
+            else
+                {   
+                   genre.gr = req.body.name;
+                   res.send(genre);
+                }    
+        }    
+}) 
+
+
+// Specific DELETE
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(element => element.id === parseInt(req.params.id));
+    if(!genre)
+        {
+            res.status(404).send("Genre Not Found");
+            return;
+        }
+    else
+        {   
+            const index = genres.indexOf(genre);
+            genres.splice(index,1);
+            res.status(200).send(genre);
+        }
 })
-
-
-
 
 app.listen(port, () => {console.log(`Node App listening on Port ${port}`)});
